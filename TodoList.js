cdacaa58aa60c09e9fdf6c459463afa7766a60ae
Raul <raul.lopezm@gmail.com>
2021-05-18 10:41:44 +0200

    TodoList: implementadas funciones crearTarea y mostrarTarea

--- TodoList.js
+++ TodoList.js
@@ -1,15 +1,22 @@
+const Todo = require('./Todo.js');
+
 export class TodoList{
 	todoList;
 
 	constructor(todos){
-		this.todoList = JSON.stringify(todos);
+		this.todoList = JSON.parse(todos);
 	}
 
-	crearTarea(todo){
-					
+	crearTarea(id,texto,fechaInicio,fechaFin,usuario,estado){
+		const tarea = new Todo(id, texto, fechaInicio, fechaFin, usuario, estado);
+		todoList.array.push(tarea);
 	}		
 
 	mostrarTarea(id){
-
+		for(let i in this.todoList){
+			if(id === this.todoList[i].id){
+				return this.todoList[i];
+			}
+		}
 	}
 }
