a0704d6854cc513736245845a539439ca3b33552
raulalhena <38526688+raulalhena@users.noreply.github.com>
2021-05-19 13:31:38 +0200

    Update TodoList.js

--- TodoList.js
+++ TodoList.js
@@ -1,15 +1,24 @@
 const Todo = require('./Todo.js');
 const { jsonReader, jsonUpdate } = require('./dbFunctions');
 
-module.exports = class TodoList{
-	constructor(){
-		this.todos = this.cargarDB();
+export default class TodoList{
+	todoList;
+
+	constructor(todos){
+		this.todoList = JSON.parse(todos);
 	}
 
-	crearTarea(texto,fechaInicio,fechaFin,usuario,estado){
-		const tarea = new Todo(texto, fechaInicio, fechaFin, usuario, estado);
-		this.todos.push(tarea);
-		this.guardarDB(this.todos);
+	crearTarea(id,texto,fechaInicio,fechaFin,usuario,estado){
+		const tarea = new Todo(id, texto, fechaInicio, fechaFin, usuario, estado);
+		todoList.array.push(tarea);
+	}		
+
+	mostrarTarea(id){
+		for(let i in this.todoList){
+			if(id === this.todoList[i].id){
+				return this.todoList[i];
+			}
+		}
 	}
 
 	mostrarTarea(id){
