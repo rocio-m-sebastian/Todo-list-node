d17c24ecfc89358a80411703c2c9f3ec90a9e369
raulalhena <38526688+raulalhena@users.noreply.github.com>
2021-05-26 13:14:01 +0200

    Update Todo.js

--- Todo.js
+++ Todo.js
@@ -1,21 +1,22 @@
-export default class Todo {
-	id;
-	texto;
-	fechaInicio;
-	fechaFin;
-	usuario;
-	estado = {
-		p: "Pendiente",
-		e: "En ejecución",
-		f: "Finalizada"
-	}
-
+module.exports = class Todo {
+	constructor(id, texto, fechaInicio, fechaFin, usuario, estado){
+		this.id = id;
 	constructor(texto, fechaInicio, fechaFin, usuario, estado){
 		this.id = new Date().getTime();
 		this.texto = texto;
 		this.fechaInicio = fechaInicio;
 		this.fechaFin = fechaFin;
 		this.usuario = usuario;
+		console.log('entra aquí');
+
+		if (estado === 3) {
+			this.estado = 'acabado';
+		} else if (estado === 2) {
+			this.estado = 'en ejecución'
+		} else {
+			console.log('entra aquí');
+			this.estado = 'pendiente'
+		}
 	}
 	
 }
